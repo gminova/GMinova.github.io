d6e48725e3621cb0326458ccca002d09bbef72b0
Gigi Minova <minova1995@gmail.com>
2019-03-11 05:20:25 +0000

    html/css/js updated to create a reactive web page

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  
+
+/* -----------------------------------------------
+                MOBILE NAVIGATION     
+-------------------------------------------------- */
+
+  $(".menu-icon").on("click", function() {
+	  if($( ".main-nav" ).hasClass( "mobile-hide" )){
+		    $(".main-nav").removeClass("mobile-hide");
+		    $(".main-nav").addClass("mobile-show");
+	  }else{
+		      $(".main-nav").addClass("mobile-hide");
+		      $(".main-nav").removeClass("mobile-show");
+	  }
+  
+  });
+
+
+});
