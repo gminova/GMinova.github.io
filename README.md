b4b5dea3745ad621161ad17f047d8e7b8456e1a0
Gigi <gigi@asus.mynet>
2019-04-12 15:21:34 +0100

    update

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
-# Portfolio --> https://gminova.github.io/
+# Website => https://gminova.github.io/
 
-My portfolio is a work in progress which aims to document my development and learning.
-## Copyright 2019 G.Minova
+This is a simple website using HTML, CSS, and a bit of raw JavaScript.  
+
+## Prerequisites
+
+1. Host it on GitHub Pages  
+2. Tell us about you  
+3. Why you are applying for the programme?  
+4. Demonstrate your learning from the prerequisites  
+5. Contain a link back to the GitHub repo that contains the code for your site  
+6. Contain links to your freeCodeCamp and Codewars profiles  
+7. Include an image carousel  
+
+```
+LET'S DO THIS!  
+```
+![Alt Text](https://media3.giphy.com/media/sIIhZliB2McAo/giphy.gif)  
+
+## Image Carousel Features
+
+```
+1. Responsive (works on mobile and desktop)  
+```
+```
+2. Display one image at a time  
+```
+```
+3. Move between images when clicking navigation buttons  
+```
+```
+4. Toggle automatically looping through all images with a play/pause button  
+```
+```
+5. Allow keyboard navigation between images with the left/right arrow keys  
+```
+
+## DO's
+
+```
+1. Earn a minimum of 250 points on freeCodeCamp  
+```
+```
+2. Reach 5 kyu and 300 honor points in JavaScript on Codewars  
+```
+
+## DON'Ts
+
+
+```
+1. Don't use a GitHub theme  
+```
+```
+2. Don't use any Javascript/CSS libraries like Bootstrap or jQuery  
+```
+```
+3. Don't use an image carousel package like Flickity  
+```
+
+### Copyright 2019 G.Minova
--- js/main.js
+++ js/main.js
@@ -1,20 +1,76 @@
-$(document).ready(function() {
-  
+//array of image sources
+let images = [
+    "/img/pic1.jpeg",
+    "/img/pic2.jpeg",
+    "/img/pic3.jpeg"
+]
 
-/* -----------------------------------------------
-                MOBILE NAVIGATION     
--------------------------------------------------- */
+let index = 0;
+var timer = null;
+let paused = false;
 
-  $(".menu-icon").on("click", function() {
-	  if($( ".main-nav" ).hasClass( "mobile-hide" )){
-		    $(".main-nav").removeClass("mobile-hide");
-		    $(".main-nav").addClass("mobile-show");
-	  }else{
-		      $(".main-nav").addClass("mobile-hide");
-		      $(".main-nav").removeClass("mobile-show");
-	  }
-  
-  });
+let nextButton = document.getElementById('buttonNext');
 
+let back = document.getElementById('buttonBack');
 
-});
+let pauseButton = document.getElementById("buttonPause");
+
+let playButton = document.getElementById("buttonPlay");
+
+window.onload = function () {
+    timer = setInterval(autoNext, 2000);
+    playButton.className = 'buttonSelected';    
+    pauseButton.className = '';
+};
+
+function pause (){
+    window.clearInterval(timer);
+    paused = true;
+    //since the user has paused the animation, I re-enable the playButton.
+    playButton.disabled = false;
+    pauseButton.className = 'buttonSelected';    
+    playButton.className = '';
+};
+
+function next() {
+    let img = document.getElementById('img');
+    index >= images.length - 1 ? index = 0 : index++;
+    img.src = images[index];
+    pause();
+};
+
+function autoNext() {
+    let img = document.getElementById('img');
+    index >= images.length - 1 ? index = 0 : index++;
+    img.src = images[index];
+};
+
+function previous() {
+    let img = document.getElementById('img');
+    index <= 0 ? index = images.length - 1 : index--;
+    img.src = images[index];
+    pause();
+};
+
+window.addEventListener('keydown', function(event){
+    if(event.code === 'ArrowRight'){
+        next();
+    }
+    else if(event.code === 'ArrowLeft'){
+        previous();
+    }
+})
+
+nextButton.onclick = next;
+back.onclick = previous; 
+pauseButton.onclick = pause;
+
+playButton.onclick = function () {
+    if (paused) {
+        timer = setInterval(autoNext, 2000);
+        paused = false;
+    }
+    playButton.disabled = true;
+    playButton.className = 'buttonSelected';    
+    pauseButton.className = '';
+};
