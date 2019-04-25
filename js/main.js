ebe11e686bcb6d2da8df0f5750ef69cc8ef8244f
gminova <minova1995@gmail.com>
2019-04-25 18:00:16 +0100

    media queries fixed, code indented

--- js/main.js
+++ js/main.js
@@ -1,76 +1,71 @@
 //array of image sources
-let images = [
-    "/img/pic1.jpg",
-    "/img/pic2.jpg",
-    "/img/pic3.jpg"
-]
+let images = ["/img/pic1.jpg", "/img/pic2.jpg", "/img/pic3.jpg"];
 
 let index = 0;
 var timer = null;
 let paused = false;
 
-let nextButton = document.getElementById('buttonNext');
+let nextButton = document.getElementById("buttonNext");
 
-let back = document.getElementById('buttonBack');
+let back = document.getElementById("buttonBack");
 
 let pauseButton = document.getElementById("buttonPause");
 
 let playButton = document.getElementById("buttonPlay");
 
-window.onload = function () {
-    timer = setInterval(autoNext, 2000);
-    playButton.className = 'button-selected';    
-    pauseButton.className = '';
+window.onload = function() {
+  timer = setInterval(autoNext, 2000);
+  playButton.className = "button-selected";
+  pauseButton.className = "";
 };
 
-function pause (){
-    window.clearInterval(timer);
-    paused = true;
-    //since the user has paused the animation, I re-enable the playButton.
-    playButton.disabled = false;
-    pauseButton.className = 'button-selected';    
-    playButton.className = '';
-};
+function pause() {
+  window.clearInterval(timer);
+  paused = true;
+  //since the user has paused the animation, I re-enable the playButton.
+  playButton.disabled = false;
+  pauseButton.className = "button-selected";
+  playButton.className = "";
+}
 
 function next() {
-    let img = document.getElementById('img');
-    index >= images.length - 1 ? index = 0 : index++;
-    img.src = images[index];
-    pause();
-};
+  let img = document.getElementById("img");
+  index >= images.length - 1 ? (index = 0) : index++;
+  img.src = images[index];
+  pause();
+}
 
 function autoNext() {
-    let img = document.getElementById('img');
-    index >= images.length - 1 ? index = 0 : index++;
-    img.src = images[index];
-};
+  let img = document.getElementById("img");
+  index >= images.length - 1 ? (index = 0) : index++;
+  img.src = images[index];
+}
 
 function previous() {
-    let img = document.getElementById('img');
-    index <= 0 ? index = images.length - 1 : index--;
-    img.src = images[index];
-    pause();
-};
+  let img = document.getElementById("img");
+  index <= 0 ? (index = images.length - 1) : index--;
+  img.src = images[index];
+  pause();
+}
 
-window.addEventListener('keydown', function(event){
-    if(event.code === 'ArrowRight'){
-        next();
-    }
-    else if(event.code === 'ArrowLeft'){
-        previous();
-    }
-})
+window.addEventListener("keydown", function(event) {
+  if (event.code === "ArrowRight") {
+    next();
+  } else if (event.code === "ArrowLeft") {
+    previous();
+  }
+});
 
 nextButton.onclick = next;
-back.onclick = previous; 
+back.onclick = previous;
 pauseButton.onclick = pause;
 
-playButton.onclick = function () {
-    if (paused) {
-        timer = setInterval(autoNext, 2000);
-        paused = false;
-    }
-    playButton.disabled = true;
-    playButton.className = 'button-selected';    
-    pauseButton.className = '';
+playButton.onclick = function() {
+  if (paused) {
+    timer = setInterval(autoNext, 2000);
+    paused = false;
+  }
+  playButton.disabled = true;
+  playButton.className = "button-selected";
+  pauseButton.className = "";
 };
